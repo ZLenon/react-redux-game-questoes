0d1751c11410718d0de2bef884fb69a4bc5602bc
Lenon <lenoninformatica@gmail.com>
2023-01-11 17:08:06 -0300

    Requisito 1

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Login extends Component {
+  state = {
+    name: '',
+    email: '',
+    isButtonDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => this.validation());
+  };
+
+  validation = () => {
+    const { name, email } = this.state;
+    const validName = name.length > 0;
+    const validEmail = email.length > 0;
+    const validBtn = validEmail && validName;
+    this.setState({
+      isButtonDisabled: !validBtn,
+    });
+  };
+
+  render() {
+    const { name, email, isButtonDisabled } = this.state;
+    return (
+      <form>
+        <label htmlFor="nome">
+          <input
+            id="nome"
+            type="text"
+            data-testid="input-player-name"
+            name="name"
+            value={ name }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="email">
+          <input
+            id="email"
+            type="text"
+            data-testid="input-gravatar-email"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+
+        <button
+          id="btn"
+          data-testid="btn-play"
+          type="button"
+          disabled={ isButtonDisabled }
+          // onClick={ this.handleClick }
+        >
+          Play
+        </button>
+
+      </form>
+    );
+  }
+}
+
+export default connect()(Login);
---
+++ src/redux/action/index.js
@@ -0,0 +1,6 @@
+export const ACTION_LOGIN = 'ACTION_LOGIN';
+
+export const actionLogin = (infoLogin) => ({
+  type: ACTION_LOGIN,
+  payload: infoLogin,
+});
