e06afa69eb76d6b593233eb378c20f769b7997ea
Pedro Machado <pedromportilho@gmail.com>
2023-01-13 16:56:14 -0300

    Requisito 14

--- src/Pages/Feedback.jsx
+++ src/Pages/Feedback.jsx
@@ -5,27 +5,33 @@ import Header from '../components/Header';
 
 class FeedBack extends Component {
   render() {
-    const { assertions } = this.props;
+    const { score, assertions } = this.props;
     const THREE = 3;
     return (
       <div>
         <Header />
+        <h1>FeedBack</h1>
         <h3
           data-testid="feedback-text"
         >
           { assertions >= THREE ? 'Well Done!' : 'Could be better...' }
         </h3>
-        <h4>FeedBack</h4>
+        <p>Acertos:</p>
+        <p data-testid="feedback-total-question">{assertions}</p>
+        <p>Pontuação:</p>
+        <p data-testid="feedback-total-score">{score}</p>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
+  score: state.player.score,
   assertions: state.player.assertions,
 });
 
 FeedBack.propTypes = {
+  score: PropTypes.number.isRequired,
   assertions: PropTypes.number.isRequired,
 };
 
